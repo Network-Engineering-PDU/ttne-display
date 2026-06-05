a13080c90e79295ffebaf18424d5d8459ded58d5
AhmedElghaly <ahmedsaad.2feb@gmail.com>
2026-06-05 15:52:18 +0300

    update the login screen

--- include/scr_login.h
+++ include/scr_login.h
@@ -1,39 +1,22 @@
-/**
- * @file scr_login.h
- * @brief Password login screen implementation using LVGL.
- * Includes password entry text area and a "Don't Request Again" checkbox.
- */
-
 #ifndef SCR_LOGIN_H
 #define SCR_LOGIN_H
+
+#include "lvgl/lvgl.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/*********************
- * INCLUDES
- *********************/
-#include "lvgl/lvgl.h"
-
-/*********************
- * PUBLIC FUNCTIONS
- *********************/
-
 /**
- * @brief Create and initialize the PDU login screen.
- * Creates a menu page in the provided LVGL menu.
+ * @brief Creates the login screen.
+ *
+ * @param[in] menu      Pointer to the menu.
+ * @param[in] btn       Main menu page button.
  */
-void scr_login_create(lv_obj_t* l_menu, lv_obj_t* btn);
-
-/**
- * @brief Check if the "Don't Request Password again" checkbox is checked.
- * @return true if checked, false otherwise.
- */
-bool scr_login_get_dont_request_again_status(void);
+void scr_login_create(lv_obj_t* menu, lv_obj_t* btn);
 
 #ifdef __cplusplus
-} /*extern "C"*/
+} /* extern "C" */
 #endif
 
-#endif /*SCR_LOGIN_H*/
+#endif /* SCR_LOGIN_H */
--- src/scr_settings_menu.c
+++ src/scr_settings_menu.c
@@ -8,8 +8,7 @@
 #include "scr_settings_sys.h"
 #include "scr_settings_upd.h"
 #include "scr_settings_nw_menu.h"
-//#include "scr_login.h"
-#include "scr_current.h"
+#include "scr_login.h"
 #include "tt_obj.h"
 #include "tt_styles.h"
 #include "tt_colors.h"
@@ -21,42 +20,13 @@
 #include "ttne_display.h"
 #include "runbg.h"
 
-#define TIMER_ROT 5000 // ms
-#define TIMER_CHECK_UPDATE 1000 // ms
-
-#ifdef SIMULATOR_ENABLED
-#define MOUNT_DIR "/home/guille"
-#else
-#define MOUNT_DIR "/run/mount"
-#endif
-
-/* Global variables ***********************************************************/
-static lv_obj_t* menu;
-
-/* Callbacks ******************************************************************/
-
-static void menu_cb(lv_event_t* e)
-{
-	lv_event_code_t code = lv_event_get_code(e);
-	lv_obj_t* obj = lv_event_get_current_target(e);
-
-	if (code == LV_EVENT_VALUE_CHANGED) {
-		lv_obj_t* curr_page = lv_event_get_user_data(e);
-		lv_obj_t* page = lv_menu_get_cur_main_page(obj);
-		if (curr_page == page) {
-			LV_LOG_USER("Settings menu cb");
-		}
-	}
-}
-
 /* Function definitions *******************************************************/
 
 void scr_settings_menu_create(lv_obj_t* l_menu, lv_obj_t* btn) 
 {
-    menu = l_menu;
 
     /* Create the settings page directly */
-    lv_obj_t* settings_page = tt_obj_menu_page_create(menu, btn, NULL, "Settings");
+    lv_obj_t* settings_page = tt_obj_menu_page_create(l_menu, btn, NULL, "Settings");
 
     /* Apply Flex layout directly to the page. 
        In LVGL, menu pages usually have a 'scrollable' part or are objects themselves 
@@ -79,12 +49,12 @@ void scr_settings_menu_create(lv_obj_t* l_menu, lv_obj_t* btn)
 
     lv_obj_t* btn_update = tt_obj_btn_mtx_create(settings_page, NULL, "Sys Update", ASSET("menu.png"));
 
+    lv_obj_t* btn_login = tt_obj_btn_mtx_create(settings_page, NULL, "Login", ASSET("menu.png"));
+
     /* Navigation links remain the same */
-    scr_current_create(menu, btn_vis);
-    //scr_settings_vis_create(menu, btn_vis);
-    scr_settings_nw_menu_create(menu, btn_nw);
-    
-    //scr_login_create(menu, btn_sys);
-    //scr_settings_sys_create(menu, btn_sys);
-    scr_settings_update_create(menu, btn_update);
+    scr_settings_vis_create(l_menu, btn_vis);
+    scr_settings_nw_menu_create(l_menu, btn_nw);
+    scr_settings_sys_create(l_menu, btn_sys);
+    scr_settings_update_create(l_menu, btn_update);
+    scr_login_create(l_menu, btn_login);
 }
