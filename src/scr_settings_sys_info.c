28bf7ff419b12d5e1a655cc4cd7b10a057906436
AhmedElghaly <ahmedsaad.2feb@gmail.com>
2026-06-09 23:07:23 +0300

    fix sys info view

--- src/scr_settings_sys_info.c
+++ src/scr_settings_sys_info.c
@@ -62,16 +62,15 @@ void scr_settings_sys_info_create(lv_obj_t* menu, lv_obj_t* btn) {
     /* Create the settings page directly */
     lv_obj_t* settings_sys_info_page = tt_obj_menu_page_create(l_menu, btn, NULL, "System Info");
     lv_obj_set_flex_flow(settings_sys_info_page, LV_FLEX_FLOW_COLUMN);
-    
-    /* Create and display the model field and value labels */
-    lv_obj_t* model_title_label = lv_label_create(settings_sys_info_page);
-    lv_label_set_text(model_title_label, "Model:");
-    lv_obj_set_style_text_font(model_title_label, &lv_font_montserrat_18, 0);
-    lv_obj_set_style_text_align(model_title_label, LV_TEXT_ALIGN_LEFT, 0);
+    lv_obj_set_flex_align(settings_sys_info_page, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
 
-    lv_obj_t* model_value_label = lv_label_create(settings_sys_info_page);
-    lv_label_set_text(model_value_label, "PowerIT Easy");
-    lv_obj_set_style_text_font(model_value_label, &lv_font_montserrat_18, 0);
-    lv_obj_set_style_text_align(model_value_label, LV_TEXT_ALIGN_LEFT, 0);
+    lv_obj_t* model_label = lv_label_create(settings_sys_info_page);
+    lv_label_set_text(model_label, "Model: PowerIT Easy");
+    lv_obj_set_style_text_font(model_label, &lv_font_montserrat_18, 0);
+    lv_obj_set_style_text_align(model_label, LV_TEXT_ALIGN_LEFT, 0);
+    lv_obj_set_width(model_label, lv_pct(100));
+    lv_obj_set_style_text_color(model_label, lv_color_white(), 0);
+    lv_obj_set_style_pad_left(model_label, 10, 0);
+    lv_obj_set_style_pad_top(model_label, 10, 0);
     
 }
