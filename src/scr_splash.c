d895824c4e9929b6ceffc79159fc8b22b56f9f0e
AhmedElghaly <ahmedsaad.2feb@gmail.com>
2026-06-10 13:47:46 +0300

    fix splash scr

--- src/scr_splash.c
+++ src/scr_splash.c
@@ -16,6 +16,7 @@
 /* Global variables ***********************************************************/
 
 static lv_obj_t* splash_scr;
+static lv_obj_t* tap_overlay;
 static lv_obj_t* menu_scr_ref;
 static lv_obj_t* login_scr_ref;
 static lv_obj_t* nav_target;
@@ -35,6 +36,7 @@ static bool flag_init = false;
 static void splash_cb(lv_event_t* e);
 static void splash_timer_cb(lv_timer_t* timer);
 static void splash_refresh_nav_target(void);
+static void splash_set_tap_enabled(bool enabled);
 static const char* get_iface_label(const models_nw_if_t* nw_if);
 
 /* Callbacks ******************************************************************/
@@ -47,9 +49,21 @@ static void splash_cb(lv_event_t* e)
 		return;
 	}
 	if (code == LV_EVENT_CLICKED && nav_target != NULL) {
-		lv_obj_clear_flag(lv_layer_top(), LV_OBJ_FLAG_CLICKABLE);
+		splash_set_tap_enabled(false);
 		lv_timer_pause(timer_check);
 		lv_scr_load(nav_target);
+	}
+}
+
+static void splash_set_tap_enabled(bool enabled)
+{
+	if (tap_overlay == NULL) {
+		return;
+	}
+	if (enabled) {
+		lv_obj_add_flag(tap_overlay, LV_OBJ_FLAG_CLICKABLE);
+	} else {
+		lv_obj_clear_flag(tap_overlay, LV_OBJ_FLAG_CLICKABLE);
 	}
 }
 
@@ -65,8 +79,9 @@ static void splash_timer_cb(lv_timer_t* timer)
 	// TODO: check a started_flag
 	if (!flag_init && strcmp(info->product_name, "N/A") != 0) { // API iniciada
 		flag_init = true;
-		lv_obj_add_flag(lv_layer_top(), LV_OBJ_FLAG_CLICKABLE);
+		splash_set_tap_enabled(true);
 		lv_obj_del(init_spinner);
+		init_spinner = NULL;
 	}
 	const char* iface = get_iface_label(nw_if);
 	sprintf(str, "%s", "PowerIT Easy");
@@ -126,13 +141,10 @@ lv_obj_t* scr_splash_create(lv_obj_t* menu_scr, lv_obj_t* login_scr)
 	lv_obj_set_style_radius(splash_scr, 0, 0);
 	lv_obj_set_style_bg_color(splash_scr, lv_color_hex(TT_COLOR_BG1), 0);
 
-	lv_obj_add_flag(splash_scr, LV_OBJ_FLAG_CLICKABLE);
-
 	lv_obj_t* logo = lv_img_create(splash_scr);
 	lv_img_set_src(logo, ASSET("ne_logo.png"));
 
 	splash_refresh_nav_target();
-	lv_obj_add_event_cb(lv_layer_top(), splash_cb, LV_EVENT_ALL, NULL);
 
 	init_spinner = tt_obj_spinner_inline_create(splash_scr,
 			"Initializing system...");
@@ -150,9 +162,28 @@ lv_obj_t* scr_splash_create(lv_obj_t* menu_scr, lv_obj_t* login_scr)
 	lbl_system = tt_obj_label_create(info_cont, NULL);
 	lbl_ip = tt_obj_label_create(info_cont, NULL);
 
+	tap_overlay = lv_obj_create(splash_scr);
+	lv_obj_remove_style_all(tap_overlay);
+	lv_obj_set_size(tap_overlay, LV_PCT(100), LV_PCT(100));
+	lv_obj_clear_flag(tap_overlay, LV_OBJ_FLAG_SCROLLABLE);
+	lv_obj_add_event_cb(tap_overlay, splash_cb, LV_EVENT_CLICKED, NULL);
+	splash_set_tap_enabled(false);
+	lv_obj_move_foreground(tap_overlay);
+
 	timer_check = lv_timer_create(splash_timer_cb, TIMER_REFRESH_RATE, NULL);
 	lv_timer_pause(timer_check);
 	splash_timer_cb(timer_check);
+
+#ifdef SIMULATOR_ENABLED
+	if (!flag_init) {
+		flag_init = true;
+		splash_set_tap_enabled(true);
+		if (init_spinner != NULL) {
+			lv_obj_del(init_spinner);
+			init_spinner = NULL;
+		}
+	}
+#endif
 
 	return splash_scr;
 }
@@ -163,7 +194,7 @@ void scr_splash_show()
 	splash_refresh_nav_target();
 	if (lv_scr_act() != splash_scr) {
 		if (flag_init) {
-			lv_obj_add_flag(lv_layer_top(), LV_OBJ_FLAG_CLICKABLE);
+			splash_set_tap_enabled(true);
 		}
 		lv_timer_resume(timer_check);
 		lv_scr_load(splash_scr);
